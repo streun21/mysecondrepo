def92233b5a228c9cb029854f0ba80ddd13978cb
Luke <Luke_Streun2@baylor.edu>
2024-01-24 15:39:18 -0600

    anything

--- script1.py
+++ script1.py
@@ -1,16 +1,16 @@
 #### PROBLEM 1 ####
-# Correc the mistakes and produce the following output:
+# Correct the mistakes and produce the following output:
 # Johnny, the total of your purchase is $1,688.70
 
-def calcTotal(amt,tax,name):
-    mytotal = amt * (1+tax)
-    print(f"{myname}, the total of your purchase is ${total:,.2f}")
+def calcTotal(amt,thetax,name):
+    total = amt * (1+thetax)
+    print(f"{name}, the total of your purchase is ${total:,.2f}")
 
-myname = 'Johnny'
-price = 1,560
-thetax = 8.25%
+name = 'Johnny'
+amt = float(1560)
+thetax = .0825
 
-calcTotal(thetax,price,name)
+calcTotal(amt, thetax, name)
 
 
 
@@ -21,7 +21,8 @@ calcTotal(thetax,price,name)
 list1 = [1,'two',3.2,'four',5]
 
 for i in list1:
-    print(i)
+     if type(i) == int or type(i) == float:
+        print(i)
 
 
 
